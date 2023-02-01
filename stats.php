ccf315b01516bf20c72aa1e5e55e13187bfed2ea
aleeiix10 <95906986+aleeiix10@users.noreply.github.com>
2023-02-01 16:58:48 +0100

    Css Added

--- stats.php
+++ stats.php
@@ -12,14 +12,11 @@ logButtonClick("S","stats.php","S'ha entrat a 'Stats' correctament\n",$_SESSION[
     $query = "select p.id,p.title as TituloEncuesta,pt.idTeacher,u.role,u.username,u.email from poll p inner join poll_teacher pt on p.id = pt.idPoll inner join creyentes_poll.user u on pt.idTeacher = u.id where p.available = 1 and email='$mail';";
     $results = getListByQuery($query);
     logButtonClick("S","View_Poll.php","$query'\n");
-    /*echo "<script>
-        var array = ".json_encode($results).";
-        console.log(array);
-    </script>";*/
-    echo "<div><ul>";
+    echo "<h1 id='usuerName'>".$_SESSION['user'][2]."</h1>";
+    echo "<div class='autocenter'>\n<div class='pollContent'>\n<ul>\n";
     for ($i=0; $i < count($results); $i++) {
-        echo "<li href=''>".$results[$i]["TituloEncuesta"]."</li>";
+        echo "<li href=''>".$results[$i]["TituloEncuesta"]."</li>\n";
     }
-    echo "</ul></div>";
+    echo "</ul>\n</div>\n</div>";
 ?>
 <?php include "footer.php"; ?>
